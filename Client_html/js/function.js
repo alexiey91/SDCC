6c124dac91c0d4cfdda099e1b44b02a46f634f38
paolosalome <paolosalome@gmail.com>
2016-11-08 13:30:10 +0100

    paolo

--- Client_html/js/function.js
+++ Client_html/js/function.js
@@ -1,6 +1,6 @@
 $(".tabellone td").click(function(){
   //alert("tabellone"+this.id);
-  console.log("hey",this.id,this.id=="");
+  //console.log("hey",this.id,this.id=="");
   if(this.id != ""){
     $('#textModal').prop('innerHTML',this.id);
     $('#tempModalQuote').prop('innerHTML',0);
@@ -25,7 +25,39 @@ $(".modal-fiches-confirm").click(function(){
     totalBet=0;
   }*/
   if(tempSum != 0){
-    $('#puntata').prop('innerHTML'," euro sul "+$('#textModal').prop('innerHTML'));
+
+    $('#confirmBetText').prop('innerHTML',"Confirm?   ");
+    $('#confirmBet').prop('hidden',false);
+    $('#puntata').prop('innerHTML'," euro on "+$('#textModal').prop('innerHTML'));
     $('#totalBet').prop('innerHTML',$('#tempModalQuote').prop('innerHTML'));
+    $('#totalBet').prev().prop('innerHTML',"Did you bet ");
   }
+});
+
+$('#confirmBet').click(function(){
+
+  $('.tabellone tr').each(function(){
+    //console.log($(this).attr('data-target'));
+    if($(this).attr('data-target')=="#modale-puntata")
+      $(this).attr('data-target',null);
+  });
+  $('#1st12').attr('data-target',null);
+  $('#2nd12').attr('data-target',null);
+  $('#3rd12').attr('data-target',null);
+  $('.tabellone').css('opacity',0.5);
+
+ setTimeout(function(){
+   console.log("+-+-+-+-timeout-+-+-+-+")
+   $('.tabellone tr').each(function(){
+     console.log($(this).attr('data-target'));
+     if($(this).attr('data-toggle')!=null)
+       $(this).attr('data-target',"#modale-puntata");
+   });
+   $('#1st12').attr('data-target',"#modale-puntata");
+   $('#2nd12').attr('data-target',"#modale-puntata");
+   $('#3rd12').attr('data-target',"#modale-puntata");
+   $('.tabellone').css('opacity',1);
+ },10000);
+
+
 });
